da3ca0e11fee531d47a88dec106d524db060c272
nxqbao <quocbao300@gmail.com>
2022-08-24 17:58:23 +0700

    Init commit ValidatorSet implementation

---
+++ contracts/ValidatorSet.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+import "./interfaces/IValidatorSet.sol";
+import "./interfaces/IStaking.sol";
+import "./ValidatorSetStorage.sol";
+
+/**
+ * @title Set of validators in current epoch
+ * @notice This contract maintains set of validator in the current epoch of Ronin network
+ */
+contract ValidatorSet is ValidatorSetStorage {
+  using EnumerableMap for EnumerableMap.AddressToUintMap;
+
+  uint256 public constant INIT_NUM_OF_CABINETS = 21;
+  uint256 public constant INIT_EPOCH_LENGTH = 200;
+
+  uint256 numOfCabinets;
+  uint256 epochLength;
+  uint256 lastUpdated;
+
+  IStaking stakingContract;
+
+  event ValidatorSetUpdated();
+  event ValidatorJailed(address indexed validator);
+  /// @dev Event for each time the validators deposit mining reward
+  event ValidatorDeposited(address indexed validator, uint256 amount);
+  /// @dev Event for the in-jail validators deposit mining reward
+  event DeprecatedDeposit(address indexed validator, uint256 amount);
+
+  modifier onlyCoinbase() {
+    require(tx.origin == block.coinbase, "Only coinbase");
+    _;
+  }
+
+  modifier onlyValidator() {
+    require(isValidator(msg.sender), "Only validator");
+    _;
+  }
+
+  modifier noEmptyDeposit() {
+    require(msg.value > 0, "No empty deposit");
+    _;
+  }
+
+  modifier atEpochEnding() {
+    require((block.number + 1) % epochLength == 0, "Only at the end of the epoch");
+    _;
+  }
+
+  constructor(IStaking _stakingContract) {
+    stakingContract = _stakingContract;
+    numOfCabinets = INIT_NUM_OF_CABINETS;
+    epochLength = INIT_EPOCH_LENGTH;
+  }
+
+  function getValidators() external view returns (address[] memory) {
+    uint256 size = currentValidatorIndexesMap.length();
+    address[] memory validatorAddresses = new address[](size);
+    for (uint256 i = 0; i < size; i++) {
+      Validator memory _v = _getValidatorAtMiningIndex(i);
+      validatorAddresses[i] = _v.consensusAddr;
+    }
+    return validatorAddresses;
+  }
+
+  function updateValidators() external onlyValidator atEpochEnding returns (address[] memory) {
+    // 1. fetch new validator set from staking contract
+    IStaking.ValidatorCandidate[] memory upcommingValidatorSet = stakingContract.updateValidatorSet();
+
+    // 2. update last updated
+    lastUpdated = block.number;
+
+    // 3. update new validator list
+    // TODO:
+  }
+
+  function getLastUpdated() external view returns (uint256 height) {
+    return lastUpdated;
+  }
+
+  function depositReward() external payable onlyCoinbase {
+    uint256 _value = msg.value;
+    address _valAddr = tx.origin;
+    Validator storage _validator = _getValidator(_valAddr);
+
+    // 1. check if validator is in current epoch
+    if (currentValidatorIndexesMap.contains(_valAddr)) {
+      // 2. check if validator is in jail
+      if (_validator.jailed) {
+        emit DeprecatedDeposit(_valAddr, _value);
+      } else {
+        stakingContract.onDeposit(_valAddr, _value);
+      }
+    }
+
+    // 3. if get deposit from deprecated validators
+    emit DeprecatedDeposit(_valAddr, _value);
+  }
+
+  function slashMisdemeanor(address validator) external {
+    revert("Unimplemented");
+  }
+
+  /// TODO:
+  function slashFelony(address validator) external {
+    revert("Unimplemented");
+  }
+
+  /// TODO:
+  function slashDoubleSign(address validator) external {
+    revert("Unimplemented");
+  }
+
+  ///
+  /// INTERNAL FUNCTIONS
+  ///
+
+  function doUpdateState(IStaking.ValidatorCandidate[] memory _incomingValidatorSet) private {
+    uint256 n = currentValidatorIndexesMap.length();
+    uint256 m = _incomingValidatorSet.length;
+    uint256 k = n < m ? n : m;
+
+    // // keep the validators that already in the exact order
+    // for (uint256 i = 0; i < k; ++i) {
+    //   Validator memory _oldValidator = validatorSet[currentValidatorIndexesMap.at(i)];
+    //   if (_oldValidator.consensusAddr != _incomingValidatorSet[i].consensusAddr) {
+    //     currentValidatorIndexesMap.set(_oldValidator.consensusAddr, 0);
+    //   }
+    // }
+
+    // // in case the current set is longer, delete trailing members in the current set
+    // if (n > m) {
+    //   for (uint256 i = m; i < n; ++i) {
+    //     Validator memory _oldValidator = validatorSet[currentValidatorIndexesMap.at(i)];
+    //     currentValidatorIndexesMap.set(_oldValidator.consensusAddr, 0);
+    //   }
+    // }
+
+    // // update the set by traversing each index
+    // for (uint256 i = 0; i < k; ++i) {
+    //   if (!_isSameValidator(_incomingValidatorSet[i], currentValidatorSet[i])) {
+    //     currentValidatorSetMap[_incomingValidatorSet[i].consensusAddr] = i;
+    //     _updateValidatorIndex(_incomingValidatorSet[i], currentValidatorSet[i]);
+    //   }
+    // }
+
+    // // update the set by appending to the current set
+    // if (m > n) {
+    //   for (uint256 i = n; i < m; ++i) {
+    //     _addOneValidator(_incomingValidatorSet[i]);
+    //   }
+    // }
+  }
+
+  function isValidator(address _addr) public view returns (bool) {
+    return validatorSetMap[_addr] > 0;
+  }
+
+  function isCurrentValidator(address _addr) public view returns (bool) {
+    return currentValidatorIndexesMap.contains(_addr);
+  }
+}
---
+++ contracts/ValidatorSetStorage.sol
@@ -0,0 +1,94 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/utils/structs/EnumerableMap.sol";
+import "./interfaces/IValidatorSet.sol";
+import "./interfaces/IStaking.sol";
+
+/**
+ * @title Set of validators in current epoch
+ * @notice This contract maintains set of validator in the current epoch of Ronin network
+ */
+abstract contract ValidatorSetStorage is IValidatorSet {
+  using EnumerableMap for EnumerableMap.AddressToUintMap;
+
+  /// @dev Array of all validators. The element at 0-slot is reserved for unknown validator.
+  Validator[] public validatorSet;
+  /// @dev Map of array of all validator
+  mapping(address => uint256) public validatorSetMap;
+  /// @dev Enumerable map of indexes of the current validator set, e.g. the array [3, 4, 1] tells
+  /// the sequence of validators to mine block in the next epoch will be at the 3-, 4- and 1-index,
+  /// One can query the mining order of an arbitrary address by this enumerable map.
+  EnumerableMap.AddressToUintMap internal currentValidatorIndexesMap;
+
+  function _setValidatorAtMiningIndex(uint256 _miningIndex, IStaking.ValidatorCandidate memory _incomingValidator)
+    internal
+  {
+    address _newValAddr = _incomingValidator.consensusAddr;
+    uint256 _index = _setValidator(_newValAddr, _incomingValidator);
+
+    (address _oldValAddr,) = currentValidatorIndexesMap.at(_miningIndex);
+    if (_oldValAddr != _newValAddr) {
+      currentValidatorIndexesMap.remove(_oldValAddr);
+      currentValidatorIndexesMap.set(_newValAddr, _index);
+    }
+  }
+
+  function _setValidator(address _valAddr, IStaking.ValidatorCandidate memory _incomingValidator)
+    internal
+    returns (uint256)
+  {
+    (bool _success, Validator storage _currentValidator) = _tryGetValidator(_valAddr);
+    if (_success) {
+      return __setExistedValidator(_incomingValidator, _currentValidator);
+    }
+    return __setUnexistentValidator(_incomingValidator);
+  }
+
+  function __setExistedValidator(
+    IStaking.ValidatorCandidate memory _incomingValidator,
+    Validator storage _currentValidator
+  ) internal returns (uint256) {
+    _currentValidator.consensusAddr = _incomingValidator.consensusAddr;
+    _currentValidator.treasuryAddr = _incomingValidator.treasuryAddr;
+
+    return validatorSetMap[_currentValidator.consensusAddr];
+  }
+
+  function __setUnexistentValidator(IStaking.ValidatorCandidate memory _incomingValidator) internal returns (uint256) {
+    uint256 index = validatorSet.length;
+
+    validatorSetMap[_incomingValidator.consensusAddr] = index;
+    Validator storage _v = validatorSet.push();
+    _v.consensusAddr = _incomingValidator.consensusAddr;
+    _v.treasuryAddr = _incomingValidator.treasuryAddr;
+
+    return index;
+  }
+
+  function _getValidatorAtMiningIndex(uint256 _miningIndex) internal view returns (Validator storage) {
+    (, uint256 _index) = currentValidatorIndexesMap.at(_miningIndex);
+    require(_index != 0, string(abi.encodePacked("Nonexistent validator at index ", _miningIndex)));
+    return validatorSet[_index];
+  }
+
+  function _getValidator(address _valAddr) internal view returns (Validator storage) {
+    (bool _success, Validator storage _v) = _tryGetValidator(_valAddr);
+    require(_success, string(abi.encodePacked("Nonexistent validator ", _valAddr)));
+    return _v;
+  }
+
+  function _tryGetValidator(address _valAddr) internal view returns (bool, Validator storage) {
+    uint256 _index = validatorSetMap[_valAddr];
+    Validator storage _v = validatorSet[_index];
+    if (_index == 0) {
+      return (false, _v);
+    }
+    return (true, _v);
+  }
+
+  function _isSameValidator(IStaking.ValidatorCandidate memory _v1, Validator memory _v2) internal pure returns (bool) {
+    return _v1.consensusAddr == _v2.consensusAddr && _v1.treasuryAddr == _v2.treasuryAddr;
+  }
+}
