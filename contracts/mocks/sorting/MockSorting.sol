ee4b910fef01b871797794bd934b7cd1fecdb449
nxqbao <quocbao300@gmail.com>
2022-09-06 13:17:42 +0700

    [Sorting] Add mock. Add test.

---
+++ contracts/mocks/sorting/MockSorting.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "../../libraries/Sorting.sol";
+
+contract MockSorting {
+  uint256[] public data;
+
+  function addData(uint256[] memory _data) public {
+    for (uint256 i; i < _data.length; i++) {
+      data.push(_data[i]);
+    }
+  }
+
+  function sort(uint256[] memory _data) public pure returns (uint256[] memory) {
+    return Sorting.sort(_data);
+  }
+
+  function sortOnStorage() public returns (uint256[] memory, uint256) {
+    uint256[] memory _tmpData = data;
+    data = Sorting.sort(_tmpData);
+
+    return (data, data.length);
+  }
+
+  function copyStorage2Memory() public view returns (uint256[] memory, uint256) {
+    uint256[] memory _tmpData = new uint256[](data.length);
+
+    for (uint256 i; i < data.length; i++) {
+      _tmpData[i] = data[i];
+    }
+
+    return (_tmpData, data.length);
+  }
+
+  function sortAddressesAndValues(address[] calldata _addrs, uint256[] calldata _values)
+    public
+    pure
+    returns (address[] memory)
+  {
+    return Sorting.sort(_addrs, _values);
+  }
+}
--- test/sorting/QuickSort.test.ts
+++ test/sorting/QuickSort.test.ts
@@ -1,29 +1,61 @@
-// import { expect } from 'chai';
-// import { deployments, ethers } from 'hardhat';
-// import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
-// import { solidityKeccak256 } from 'ethers/lib/utils';
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
 
-// import {
-//   QuickSort,
-//   QuickSort__factory,
-//   QuickSortMock,
-//   QuickSortMock__factory
-// } from '../../src/types';
-// import { BigNumber } from 'ethers/lib/ethers';
+import { Sorting, Sorting__factory, MockSorting, MockSorting__factory } from '../../src/types';
 
-// let quickSortLib: QuickSort;
-// let quickSortContract: QuickSortMock;
+let quickSortLib: Sorting;
+let quickSortContract: MockSorting;
 
-// let admin: SignerWithAddress;
+let deployer: SignerWithAddress;
+let signers: SignerWithAddress[];
 
-// describe('Quick sort', () => {
-//   before(async () => {
-//     [admin] = await ethers.getSigners();
-//     quickSortLib = await new QuickSort__factory().deploy();
-//     quickSortContract = await new QuickSortMock__factory(admin).deploy();
-//   });
+const runSortWithNRecords = async (numOfRecords: number) => {
+  let balances = [];
+  for (let i = 0; i < numOfRecords; ++i) {
+    balances.push({
+      address: signers[i].address,
+      value: Math.floor(Math.random() * numOfRecords * 1000),
+    });
+  }
 
-//   describe('Operation functions', async () => {
-    
-//   });
-// });
+  balances.sort((a, b) => (a.value < b.value ? 1 : -1));
+
+  let sorted = await quickSortContract.sortAddressesAndValues(
+    balances.map((_) => _.address),
+    balances.map((_) => _.value)
+  );
+
+  expect(sorted).eql(balances.map((_) => _.address));
+};
+
+describe('Quick sort', () => {
+  before(async () => {
+    [deployer, ...signers] = await ethers.getSigners();
+    quickSortLib = await new Sorting__factory(deployer).deploy();
+    quickSortContract = await new MockSorting__factory(
+      {
+        'contracts/libraries/Sorting.sol:Sorting': quickSortLib.address,
+      },
+      deployer
+    ).deploy();
+  });
+
+  describe('Sorting on sort(address[], uint[])', async () => {
+    it('Should sort correctly on 10 records', async () => {
+      await runSortWithNRecords(10);
+    });
+
+    it('Should sort correctly on 21 records', async () => {
+      await runSortWithNRecords(21);
+    });
+
+    it('Should sort correctly on 50 records', async () => {
+      await runSortWithNRecords(50);
+    });
+
+    it('Should sort correctly on 99 records', async () => {
+      await runSortWithNRecords(99);
+    });
+  });
+});
