42c1ff436998ea94696e90baf1383c05830a5e2a
nxqbao <quocbao300@gmail.com>
2022-09-13 18:02:15 +0700

    [IntegrationTest] Add test on contract constructor parameters

--- test/integration/Configuration.test.ts
+++ test/integration/Configuration.test.ts
@@ -34,6 +34,8 @@ const slashFelonyAmount = BigNumber.from(1);
 const slashDoubleSignAmount = 1000;
 const maxValidatorNumber = 4;
 const minValidatorBalance = BigNumber.from(100);
+const numberOfBlocksInEpoch = 600;
+const numberOfEpochsInPeriod = 48;
 const felonyJailDuration = 28800 * 2;
 const misdemeanorThreshold = 10;
 const felonyThreshold = 20;
@@ -56,9 +58,9 @@ describe('[Integration] Configuration check', () => {
         felonyJailBlocks: felonyJailDuration,
       };
       roninValidatorSetConf[network.name] = {
-        maxValidatorNumber: 21,
-        numberOfBlocksInEpoch: 600,
-        numberOfEpochsInPeriod: 48, // 1 day
+        maxValidatorNumber: maxValidatorNumber,
+        numberOfBlocksInEpoch: numberOfBlocksInEpoch,
+        numberOfEpochsInPeriod: numberOfEpochsInPeriod,
       };
       stakingConfig[network.name] = {
         minValidatorBalance: minValidatorBalance,
@@ -92,6 +94,21 @@ describe('[Integration] Configuration check', () => {
       let _slashingContract = await validatorContract.slashIndicatorContract();
       expect(_slashingContract).to.eq(slashContract.address);
     });
+
+    it.skip('Should config the maxValidatorNumber correctly', async () => {
+      // let _maxValidatorNumber= await validatorContract.maxValidatorNumber();
+      // expect(_maxValidatorNumber).to.eq(maxValidatorNumber);
+    });
+
+    it('Should config the numberOfBlocksInEpoch correctly', async () => {
+      let _numberOfBlocksInEpoch = await validatorContract.numberOfBlocksInEpoch();
+      expect(_numberOfBlocksInEpoch).to.eq(numberOfBlocksInEpoch);
+    });
+
+    it('Should config the numberOfEpochsInPeriod correctly', async () => {
+      let _numberOfEpochsInPeriod = await validatorContract.numberOfEpochsInPeriod();
+      expect(_numberOfEpochsInPeriod).to.eq(numberOfEpochsInPeriod);
+    });
   });
 
   describe('StakingContract configuration', async () => {
@@ -99,12 +116,47 @@ describe('[Integration] Configuration check', () => {
       let _validatorSetContract = await stakingContract.validatorContract();
       expect(_validatorSetContract).to.eq(validatorContract.address);
     });
+
+    it('Should config the minValidatorBalance correctly', async () => {
+      let _minValidatorBalance = await stakingContract.minValidatorBalance();
+      expect(_minValidatorBalance).to.eq(minValidatorBalance);
+    });
+
+    it('Should config the maxValidatorCandidate correctly', async () => {
+      let _maxValidatorCandidate = await stakingContract.maxValidatorCandidate();
+      expect(_maxValidatorCandidate).to.eq(maxValidatorNumber);
+    });
   });
 
   describe('SlashIndicatorContract configuration', async () => {
     it('Should the SlashIndicatorContract config the ValidatorSetContract correctly', async () => {
       let _validatorSetContract = await slashContract.validatorContract();
       expect(_validatorSetContract).to.eq(validatorContract.address);
+    });
+
+    it('Should config the misdemeanorThreshold correctly', async () => {
+      let _misdemeanorThreshold = await slashContract.misdemeanorThreshold();
+      expect(_misdemeanorThreshold).to.eq(misdemeanorThreshold);
+    });
+
+    it('Should config the felonyThreshold correctly', async () => {
+      let _felonyThreshold = await slashContract.felonyThreshold();
+      expect(_felonyThreshold).to.eq(felonyThreshold);
+    });
+
+    it('Should config the slashFelonyAmount correctly', async () => {
+      let _slashFelonyAmount = await slashContract.slashFelonyAmount();
+      expect(_slashFelonyAmount).to.eq(slashFelonyAmount);
+    });
+
+    it('Should config the slashDoubleSignAmount correctly', async () => {
+      let _slashDoubleSignAmount = await slashContract.slashDoubleSignAmount();
+      expect(_slashDoubleSignAmount).to.eq(slashDoubleSignAmount);
+    });
+
+    it('Should config the felonyJailDuration correctly', async () => {
+      let _felonyJailDuration = await slashContract.felonyJailDuration();
+      expect(_felonyJailDuration).to.eq(felonyJailDuration);
     });
   });
 });
