b412f8adbe7e6656b999cd5eb61d3195e07881c6
Duc Tho Tran <ducthotran2010@gmail.com>
2022-10-15 16:17:39 +0700

    Update RoninTrustedOrganization contract

--- contracts/multi-chains/RoninTrustedOrganization.sol
+++ contracts/multi-chains/RoninTrustedOrganization.sol
@@ -10,32 +10,93 @@ import "../extensions/collections/HasProxyAdmin.sol";
 contract RoninTrustedOrganization is IRoninTrustedOrganization, HasProxyAdmin, Initializable {
   using EnumerableSet for EnumerableSet.AddressSet;
 
+  uint256 internal _num;
+  uint256 internal _denom;
+  uint256 internal _totalWeights;
+  uint256 internal _nonce;
+
+  /// @dev Address set of the trusted organizations
   EnumerableSet.AddressSet internal _orgs;
+  /// @dev Mapping from trusted organization address => its weight
+  mapping(address => uint256) internal _weight;
 
   /**
    * @dev Initializes the contract storage.
    */
-  function initialize(address[] calldata _trustedOrgs) external initializer {
+  function initialize(
+    WeightedAddress[] calldata _trustedOrgs,
+    uint256 __num,
+    uint256 __denom
+  ) external initializer {
     _addTrustedOrganizations(_trustedOrgs);
+    _setThreshold(__num, __denom);
+  }
+
+  /**
+   * @inheritdoc IQuorum
+   */
+  function getThreshold() external view virtual returns (uint256, uint256) {
+    return (_num, _denom);
+  }
+
+  /**
+   * @inheritdoc IQuorum
+   */
+  function checkThreshold(uint256 _voteWeight) external view virtual returns (bool) {
+    return _voteWeight * _denom >= _num * _totalWeights;
+  }
+
+  /**
+   * @inheritdoc IQuorum
+   */
+  function minimumVoteWeight() external view virtual returns (uint256) {
+    return (_num * _totalWeights + _denom - 1) / _denom;
+  }
+
+  /**
+   * @inheritdoc IQuorum
+   */
+  function setThreshold(uint256 _numerator, uint256 _denominator)
+    external
+    override
+    onlyAdmin
+    returns (uint256, uint256)
+  {
+    return _setThreshold(_numerator, _denominator);
   }
 
   /**
    * @inheritdoc IRoninTrustedOrganization
    */
-  function addTrustedOrganizations(address[] calldata _list) external override onlyAdmin {
+  function addTrustedOrganizations(WeightedAddress[] calldata _list) external override onlyAdmin {
     _addTrustedOrganizations(_list);
   }
 
   /**
    * @inheritdoc IRoninTrustedOrganization
    */
-  function removeTrustedOrganizations(address[] calldata _list) external override onlyAdmin {
-    if (_list.length == 0) {
-      return;
-    }
+  function updateTrustedOrganizations(WeightedAddress[] calldata _list) external override onlyAdmin {
+    WeightedAddress memory _item;
+    for (uint _i = 0; _i < _list.length; _i++) {
+      _item = _list[_i];
 
+      if (_orgs.contains(_item.addr) && _item.weight > 0) {
+        _totalWeights -= _weight[_item.addr];
+        _totalWeights += _item.weight;
+        _weight[_item.addr] = _item.weight;
+        emit TrustedOrganizationUpdated(_item);
+      }
+    }
+  }
+
+  /**
+   * @inheritdoc IRoninTrustedOrganization
+   */
+  function removeTrustedOrganizations(address[] calldata _list) external override onlyAdmin {
     for (uint _i = 0; _i < _list.length; _i++) {
       if (_orgs.remove(_list[_i])) {
+        _totalWeights -= _weight[_list[_i]];
+        delete _weight[_list[_i]];
         emit TrustedOrganizationRemoved(_list[_i]);
       }
     }
@@ -44,18 +105,42 @@ contract RoninTrustedOrganization is IRoninTrustedOrganization, HasProxyAdmin, I
   /**
    * @inheritdoc IRoninTrustedOrganization
    */
-  function isTrustedOrganizations(address[] calldata _list) external view override returns (bool[] memory _res) {
-    _res = new bool[](_list.length);
+  function totalWeights() external view virtual returns (uint256) {
+    return _totalWeights;
+  }
+
+  /**
+   * @inheritdoc IRoninTrustedOrganization
+   */
+  function getWeight(address _addr) external view override returns (uint256) {
+    return _weight[_addr];
+  }
+
+  /**
+   * @inheritdoc IRoninTrustedOrganization
+   */
+  function getWeights(address[] calldata _list) external view override returns (uint256[] memory _res) {
+    _res = new uint256[](_list.length);
     for (uint _i = 0; _i < _res.length; _i++) {
-      _res[_i] = _orgs.contains(_list[_i]);
+      _res[_i] = _weight[_list[_i]];
     }
   }
 
   /**
    * @inheritdoc IRoninTrustedOrganization
    */
-  function getTrustedOrganizationAt(uint256 _idx) external view override returns (address) {
-    return _orgs.at(_idx);
+  function sumWeights(address[] calldata _list) external view override returns (uint256 _res) {
+    for (uint _i = 0; _i < _list.length; _i++) {
+      _res += _weight[_list[_i]];
+    }
+  }
+
+  /**
+   * @inheritdoc IRoninTrustedOrganization
+   */
+  function getTrustedOrganizationAt(uint256 _idx) external view override returns (WeightedAddress memory _res) {
+    _res.addr = _orgs.at(_idx);
+    _res.weight = _weight[_res.addr];
   }
 
   /**
@@ -68,22 +153,44 @@ contract RoninTrustedOrganization is IRoninTrustedOrganization, HasProxyAdmin, I
   /**
    * @inheritdoc IRoninTrustedOrganization
    */
-  function getAllTrustedOrganizations() external view override returns (address[] memory) {
-    return _orgs.values();
+  function getAllTrustedOrganizations() external view override returns (WeightedAddress[] memory _res) {
+    address[] memory _list = _orgs.values();
+    _res = new WeightedAddress[](_list.length);
+    for (uint _i = 0; _i < _res.length; _i++) {
+      _res[_i].addr = _list[_i];
+      _res[_i].weight = _weight[_list[_i]];
+    }
   }
 
   /**
    * @dev Adds a list of addresses into the trusted organization.
    */
-  function _addTrustedOrganizations(address[] calldata _list) internal {
-    if (_list.length == 0) {
-      return;
-    }
-
+  function _addTrustedOrganizations(WeightedAddress[] calldata _list) internal {
     for (uint _i = 0; _i < _list.length; _i++) {
-      if (_orgs.add(_list[_i])) {
+      if (_orgs.add(_list[_i].addr) && _list[_i].weight > 0) {
+        _totalWeights += _list[_i].weight;
+        _weight[_list[_i].addr] = _list[_i].weight;
         emit TrustedOrganizationAdded(_list[_i]);
       }
     }
+  }
+
+  /**
+   * @dev Sets threshold and returns the old one.
+   *
+   * Emits the `ThresholdUpdated` event.
+   *
+   */
+  function _setThreshold(uint256 _numerator, uint256 _denominator)
+    internal
+    virtual
+    returns (uint256 _previousNum, uint256 _previousDenom)
+  {
+    require(_numerator <= _denominator, "RoninTrustedOrganization: invalid threshold");
+    _previousNum = _num;
+    _previousDenom = _denom;
+    _num = _numerator;
+    _denom = _denominator;
+    emit ThresholdUpdated(_nonce++, _numerator, _denominator, _previousNum, _previousDenom);
   }
 }
