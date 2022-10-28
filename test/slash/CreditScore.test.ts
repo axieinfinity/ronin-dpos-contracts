1f5cb60741a00ef08d005d1a511ac95dfaca2125
nxqbao <quocbao300@gmail.com>
2022-10-28 11:32:08 +0700

    Add more test for credit score

--- test/slash/CreditScore.test.ts
+++ test/slash/CreditScore.test.ts
@@ -1,4 +1,4 @@
-import { BigNumber, BytesLike } from 'ethers';
+import { BigNumber, BytesLike, Transaction } from 'ethers';
 import { expect } from 'chai';
 import { ethers, network } from 'hardhat';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
@@ -17,6 +17,7 @@ import { initTest } from '../helpers/fixture';
 import { EpochController } from '../helpers/ronin-validator-set';
 import { IndicatorController, ScoreController } from '../helpers/slash';
 import { GovernanceAdminInterface } from '../../src/script/governance-admin-interface';
+import { SlashType } from '../../src/script/slash-indicator';
 
 let slashContract: MockSlashIndicatorExtended;
 let mockSlashLogic: MockSlashIndicatorExtended;
@@ -40,18 +41,59 @@ const gainCreditScore = 50;
 const maxCreditScore = 600;
 const bailOutCostMultiplier = 5;
 
+const misdemeanorThreshold = 5;
+const felonyThreshold = 15;
+const slashFelonyAmount = 2;
+
 const minValidatorBalance = BigNumber.from(100);
 const maxValidatorNumber = 5;
 const numberOfBlocksInEpoch = 600;
 const minOffset = 200;
 
-const endPeriodAndWrapUp = async (includingEpochsNum?: number) => {
-  includingEpochsNum = includingEpochsNum ?? 1;
+const wrapUpEpoch = async () => {
+  await localEpochController.mineToBeforeEndOfEpoch();
+  await network.provider.send('hardhat_setCoinbase', [coinbase.address]);
+  await validatorContract.connect(coinbase).wrapUpEpoch();
+};
+
+const endPeriodAndWrapUpAndResetIndicators = async (includingEpochsNum?: number) => {
+  if (includingEpochsNum) {
+    expect(includingEpochsNum).gt(0);
+  }
+
+  includingEpochsNum = includingEpochsNum ? includingEpochsNum - 1 : 0;
   for (let i = 0; i < includingEpochsNum; i++) {
     await localEpochController.mineToBeforeEndOfEpoch();
   }
   await EpochController.setTimestampToPeriodEnding();
-  await validatorContract.connect(coinbase).wrapUpEpoch();
+  await wrapUpEpoch();
+
+  validatorCandidates.map((_, i) => localIndicatorController.resetAt(i));
+};
+
+const slashUntilValidatorTier = async (slasherIdx: number, slasheeIdx: number, tier: number) => {
+  if (tier != 1 && tier != 2) {
+    return;
+  }
+
+  let _threshold = tier == 1 ? misdemeanorThreshold : felonyThreshold;
+  let _slashType = tier == 1 ? SlashType.MISDEMEANOR : SlashType.FELONY;
+
+  let tx;
+  let slasher = validatorCandidates[slasherIdx];
+  let slashee = validatorCandidates[slasheeIdx];
+
+  await network.provider.send('hardhat_setCoinbase', [slasher.address]);
+
+  let _toSlashTimes = _threshold - localIndicatorController.getAt(slasheeIdx);
+
+  for (let i = 0; i < _toSlashTimes; i++) {
+    tx = await slashContract.connect(slasher).slash(slashee.address);
+  }
+
+  let period = await validatorContract.currentPeriod();
+  await expect(tx).to.emit(slashContract, 'UnavailabilitySlashed').withArgs(slashee.address, _slashType, period);
+  localIndicatorController.setAt(slasheeIdx, _threshold);
 };
 
 const validateScoreAt = async (idx: number) => {
@@ -62,7 +104,7 @@ describe('Credit score and bail out test', () => {
   before(async () => {
     [deployer, coinbase, governor, vagabond, ...validatorCandidates] = await ethers.getSigners();
 
-    candidateAdmins = validatorCandidates.slice(maxValidatorNumber);
+    candidateAdmins = validatorCandidates.slice(0, maxValidatorNumber);
     validatorCandidates = validatorCandidates.slice(maxValidatorNumber, maxValidatorNumber * 2);
 
     const { slashContractAddress, stakingContractAddress, validatorContractAddress, roninGovernanceAdminAddress } =
@@ -81,6 +123,9 @@ describe('Credit score and bail out test', () => {
         maxValidatorNumber,
         numberOfBlocksInEpoch,
         minOffset,
+        misdemeanorThreshold,
+        felonyThreshold,
+        slashFelonyAmount,
       });
 
     stakingContract = Staking__factory.connect(stakingContractAddress, deployer);
@@ -97,8 +142,7 @@ describe('Credit score and bail out test', () => {
     await mockSlashLogic.deployed();
     await governanceAdminInterface.upgrade(slashContractAddress, mockSlashLogic.address);
 
-    validatorCandidates = validatorCandidates.slice(0, maxValidatorNumber);
-    for (let i = 0; i < maxValidatorNumber; i++) {
+    for (let i = 0; i < 2; i++) {
       await stakingContract
         .connect(validatorCandidates[i])
         .applyValidatorCandidate(
@@ -107,7 +151,7 @@ describe('Credit score and bail out test', () => {
           validatorCandidates[i].address,
           validatorCandidates[i].address,
           1,
-          { value: minValidatorBalance.mul(2).add(maxValidatorNumber).sub(i) }
+          { value: minValidatorBalance.mul(2).sub(i) }
         );
     }
 
@@ -116,26 +160,44 @@ describe('Credit score and bail out test', () => {
     localEpochController = new EpochController(minOffset, numberOfBlocksInEpoch);
     await localEpochController.mineToBeforeEndOfEpoch();
     await validatorContract.connect(coinbase).wrapUpEpoch();
-    expect(await validatorContract.getValidators()).eql(validatorCandidates.map((_) => _.address));
-    expect(await validatorContract.getBlockProducers()).eql(validatorCandidates.map((_) => _.address));
+    expect(await validatorContract.getValidators()).eql(validatorCandidates.slice(0, 2).map((_) => _.address));
+    expect(await validatorContract.getBlockProducers()).eql(validatorCandidates.slice(0, 2).map((_) => _.address));
 
     localIndicatorController = new IndicatorController(validatorCandidates.length);
     localScoreController = new ScoreController(validatorCandidates.length);
   });
 
   describe('Counting credit score after each period', async () => {
-    it('Should the score updated correctly, case: in jail (N), max score (N), unavailability (N)', async () => {
-      await endPeriodAndWrapUp();
+    it('Should the score updated correctly, case: max score (N), in jail (N), unavailability (N)', async () => {
+      await endPeriodAndWrapUpAndResetIndicators();
       localScoreController.increaseAtWithUpperbound(0, maxCreditScore, gainCreditScore);
       await validateScoreAt(0);
     });
-    it('Should the score updated correctly, case: in jail (N), max score (N), unavailability (y)', async () => {});
-    it('Should the score updated correctly, case: in jail (N), max score (y), unavailability (N)', async () => {});
-    it('Should the score updated correctly, case: in jail (N), max score (y), unavailability (y)', async () => {});
-    it('Should the score updated correctly, case: in jail (y), max score (N), unavailability (N)', async () => {});
-    it('Should the score updated correctly, case: in jail (y), max score (N), unavailability (y)', async () => {});
-    it('Should the score updated correctly, case: in jail (y), max score (y), unavailability (N)', async () => {});
-    it('Should the score updated correctly, case: in jail (y), max score (y), unavailability (y)', async () => {});
+    it('Should the score updated correctly, case: max score (N), in jail (N), unavailability (y)', async () => {
+      await network.provider.send('hardhat_setCoinbase', [validatorCandidates[1].address]);
+      await slashContract.connect(validatorCandidates[1]).slash(validatorCandidates[0].address);
+      localIndicatorController.increaseAt(1);
+      await endPeriodAndWrapUpAndResetIndicators();
+      localScoreController.increaseAtWithUpperbound(0, maxCreditScore, gainCreditScore - 1);
+      await validateScoreAt(0);
+    });
+    it('Should the score updated correctly, case: max score (N), in jail (y), unavailability (N)', async () => {
+      await slashUntilValidatorTier(1, 0, 2);
+      await wrapUpEpoch();
+      await endPeriodAndWrapUpAndResetIndicators();
+      localScoreController.increaseAtWithUpperbound(0, maxCreditScore, 0);
+      await validateScoreAt(0);
+
+      let _jailLeft = await validatorContract.jailedTimeLeft(validatorCandidates[0].address);
+      network.provider.send('hardhat_mine', [_jailLeft.blockLeft_.toHexString(), '0x0']);
+    });
+    it('Should the score updated correctly, case: max score (y), in jail (N), unavailability (N)', async () => {
+      for (let i = 0; i < maxCreditScore / gainCreditScore + 1; i++) {
+        await endPeriodAndWrapUpAndResetIndicators();
+        localScoreController.increaseAtWithUpperbound(0, maxCreditScore, gainCreditScore);
+        await validateScoreAt(0);
+      }
+    });
   });
 
   describe('Bail out test', async () => {
