bfe546b9ad3f8bc847333d66e3a90de253c0668d
nxqbao <quocbao300@gmail.com>
2022-11-09 18:14:34 +0700

    Add configs for testnet

--- src/config.ts
+++ src/config.ts
@@ -33,6 +33,10 @@ export const generalRoninConf: GeneralConfig = {
     startedAtBlock: 0,
     bridgeContract: ethers.constants.AddressZero,
   },
+  [Network.Testnet]: {
+    startedAtBlock: 0,
+    bridgeContract: ethers.constants.AddressZero,
+  },
 };
 
 export const generalMainchainConf: GeneralConfig = {
@@ -41,6 +45,10 @@ export const generalMainchainConf: GeneralConfig = {
     bridgeContract: ethers.constants.AddressZero,
   },
   [Network.Devnet]: {
+    startedAtBlock: 0,
+    bridgeContract: ethers.constants.AddressZero,
+  },
+  [Network.Testnet]: {
     startedAtBlock: 0,
     bridgeContract: ethers.constants.AddressZero,
   },
@@ -55,7 +63,12 @@ export const maintenanceConf: MaintenanceConfig = {
     minOffset: 28800, // requests before maintaining at least ~1 day
     maxSchedules: 3, // only 3 schedules are happening|in the futures
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    minMaintenanceBlockPeriod: 600, // 600 blocks
+    maxMaintenanceBlockPeriod: 28800, // ~1 day
+    minOffset: 28800, // requests before maintaining at least ~1 day
+    maxSchedules: 3, // only 3 schedules are happening|in the futures
+  },
   [Network.Mainnet]: undefined,
 };
 
@@ -67,7 +80,11 @@ export const stakingConfig: StakingConfig = {
     cooldownSecsToUndelegate: 3 * 86400, // at least 3 days
     waitingSecsToRevoke: 7 * 86400, // at least 7 days
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    minValidatorStakingAmount: BigNumber.from(10).pow(18).mul(BigNumber.from(10).pow(5).mul(5)), // 500.000 RON
+    cooldownSecsToUndelegate: 3 * 86400, // at least 3 days
+    waitingSecsToRevoke: 7 * 86400, // at least 7 days
+  },
   [Network.Mainnet]: undefined,
 };
 
@@ -79,7 +96,11 @@ export const stakingVestingConfig: StakingVestingConfig = {
     bridgeOperatorBonusPerBlock: BigNumber.from(10).pow(18), // 1 RON per block
     topupAmount: BigNumber.from(10).pow(18).mul(BigNumber.from(10).pow(4)), // 10.000 RON
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    blockProducerBonusPerBlock: BigNumber.from(10).pow(18), // 1 RON per block
+    bridgeOperatorBonusPerBlock: BigNumber.from(10).pow(18), // 1 RON per block
+    topupAmount: BigNumber.from(10).pow(18).mul(BigNumber.from(10).pow(5)), // 100.000 RON
+  },
   [Network.Mainnet]: undefined,
 };
 
@@ -113,7 +134,33 @@ export const slashIndicatorConf: SlashIndicatorConfig = {
       cutOffPercentageAfterBailout: 50_00, // 50%
     },
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    bridgeOperatorSlashing: {
+      missingVotesRatioTier1: 10_00, // 10%
+      missingVotesRatioTier2: 20_00, // 20%
+      jailDurationForMissingVotesRatioTier2: 28800 * 2, // jails for 2 days
+    },
+    bridgeVotingSlashing: {
+      bridgeVotingThreshold: 28800 * 3, // ~3 days
+      bridgeVotingSlashAmount: BigNumber.from(10).pow(18).mul(10_000), // 10.000 RON
+    },
+    doubleSignSlashing: {
+      slashDoubleSignAmount: BigNumber.from(10).pow(18).mul(BigNumber.from(10).pow(5).mul(5)), // 500.000 RON
+      doubleSigningJailUntilBlock: ethers.constants.MaxUint256,
+    },
+    unavailabilitySlashing: {
+      unavailabilityTier1Threshold: 50,
+      unavailabilityTier2Threshold: 150,
+      slashAmountForUnavailabilityTier2Threshold: BigNumber.from(10).pow(18).mul(BigNumber.from(10).pow(4)), // 10.000 RON
+      jailDurationForUnavailabilityTier2Threshold: 2 * 28800, // jails for 2 days
+    },
+    creditScore: {
+      gainCreditScore: 50,
+      maxCreditScore: 600,
+      bailOutCostMultiplier: 5,
+      cutOffPercentageAfterBailout: 50_00, // 50%
+    },
+  },
   [Network.Mainnet]: undefined,
 };
 
@@ -126,7 +173,12 @@ export const roninValidatorSetConf: RoninValidatorSetConfig = {
     maxValidatorCandidate: 100,
     numberOfBlocksInEpoch: 600,
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    maxValidatorNumber: 21,
+    maxPrioritizedValidatorNumber: 11,
+    maxValidatorCandidate: 100,
+    numberOfBlocksInEpoch: 200,
+  },
   [Network.Mainnet]: undefined,
 };
 
@@ -144,7 +196,18 @@ export const roninTrustedOrganizationConf: RoninTrustedOrganizationConfig = {
     numerator: 0,
     denominator: 1,
   },
-  [Network.Testnet]: undefined,
+  [Network.Testnet]: {
+    trustedOrganizations: [].map((addr) => ({
+      // TODO: @minh-bq
+      consensusAddr: addr,
+      governor: addr,
+      bridgeVoter: addr,
+      weight: 100,
+      addedBlock: 0,
+    })),
+    numerator: 9,
+    denominator: 11,
+  },
   [Network.Mainnet]: undefined,
   [Network.Goerli]: undefined,
   [Network.Ethereum]: undefined,
@@ -157,6 +220,10 @@ export const mainchainGovernanceAdminConf: MainchainGovernanceAdminConfig = {
     roleSetter: '0x93b8eed0a1e082ae2f478fd7f8c14b1fc0261bb1',
     relayers: ['0x93b8eed0a1e082ae2f478fd7f8c14b1fc0261bb1'],
   },
-  [Network.Goerli]: undefined,
+  [Network.Goerli]: {
+    roleSetter: '', // TODO
+    relayers: [''],
+    TODO,
+  },
   [Network.Ethereum]: undefined,
 };
