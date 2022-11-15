0215c24550b5fc8ab0ac9ba98c59acfe01e59ee5
Duc Tho Tran <ducthotran2010@gmail.com>
2022-11-15 17:02:34 +0700

    Add deployment artifacts

--- src/deploy/logic/ronin-gateway-v2.ts
+++ src/deploy/logic/ronin-gateway-v2.ts
@@ -1,23 +1,23 @@
 import { network } from 'hardhat';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-import { mainchainNetworks } from '../../config';
+import { roninchainNetworks } from '../../config';
 
 const deploy = async ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) => {
-  if (!mainchainNetworks.includes(network.name!)) {
+  if (!roninchainNetworks.includes(network.name!)) {
     return;
   }
 
   const { deploy } = deployments;
   const { deployer } = await getNamedAccounts();
 
-  await deploy('MainchainGatewayV2Logic', {
-    contract: 'MainchainGatewayV2',
+  await deploy('RoninGatewayV2Logic', {
+    contract: 'RoninGatewayV2',
     from: deployer,
     log: true,
   });
 };
 
-deploy.tags = ['MainchainGatewayV2Logic'];
+deploy.tags = ['RoninGatewayV2Logic'];
 
 export default deploy;
