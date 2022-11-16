1029d9956245dae4f3e66e3da30a7e9a62cd4220
nxqbao <quocbao300@gmail.com>
2022-11-16 16:14:16 +0700

    Merge interface

--- contracts/interfaces/validator/IRoninValidatorSet.sol
+++ contracts/interfaces/validator/IRoninValidatorSet.sol
@@ -3,17 +3,8 @@
 pragma solidity ^0.8.9;
 
 import "./ICandidateManager.sol";
-import "./info-fragments/IJailingInfo.sol";
-import "./info-fragments/ITimingInfo.sol";
-import "./info-fragments/IValidatorInfo.sol";
+import "./info-fragments/ICommonInfo.sol";
 import "./ICoinbaseExecution.sol";
 import "./ISlashingExecution.sol";
 
-interface IRoninValidatorSet is
-  ITimingInfo,
-  IJailingInfo,
-  ICandidateManager,
-  IValidatorInfo,
-  ISlashingExecution,
-  ICoinbaseExecution
-{}
+interface IRoninValidatorSet is ICandidateManager, ICommonInfo, ISlashingExecution, ICoinbaseExecution {}
