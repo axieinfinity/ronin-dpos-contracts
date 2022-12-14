adc6bfa6a08d41098c8cf1a756973bfaa2788c8a
nxqbao <quocbao300@gmail.com>
2022-12-14 16:06:10 +0700

    Add override

--- contracts/ronin/staking/BaseStaking.sol
+++ contracts/ronin/staking/BaseStaking.sol
@@ -55,7 +55,7 @@ abstract contract BaseStaking is
   /**
    * @inheritdoc IBaseStaking
    */
-  function isActivePoolAdmin(address _poolAdminAddr) public view returns (bool) {
+  function isActivePoolAdmin(address _poolAdminAddr) public view override returns (bool) {
     return _activePoolAdminMapping[_poolAdminAddr] != address(0);
   }
 
