410837bcd5bac8ca3fba6e30f0cff88dae77b309
nxqbao <quocbao300@gmail.com>
2023-02-20 14:44:39 +0700

    update contract

--- contracts/ronin/validator/storage-fragments/TimingStorage.sol
+++ contracts/ronin/validator/storage-fragments/TimingStorage.sol
@@ -44,7 +44,7 @@ abstract contract TimingStorage is ITimingInfo {
    * @inheritdoc ITimingInfo
    */
   function tryGetPeriodOfEpoch(uint256 _epoch) external view returns (bool _filled, uint256 _periodNumber) {
-    return (_periodOf[_epoch] > 0, _periodOf[_epoch]);
+    return (_epoch <= epochOf(block.number) || _periodOf[_epoch] > 0, _periodOf[_epoch]);
   }
 
   /**
