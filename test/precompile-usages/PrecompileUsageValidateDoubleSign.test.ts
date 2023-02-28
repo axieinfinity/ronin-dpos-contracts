5ff6892e536389360da87c4b0e58f92159e87c97
nxqbao <quocbao300@gmail.com>
2023-02-28 12:25:36 +0700

    fix test

--- test/precompile-usages/PrecompileUsageValidateDoubleSign.test.ts
+++ test/precompile-usages/PrecompileUsageValidateDoubleSign.test.ts
@@ -8,6 +8,7 @@ import {
   MockPCUValidateDoubleSign,
   MockPCUValidateDoubleSign__factory,
 } from '../../src/types';
+import { Address } from 'hardhat-deploy/dist/types';
 
 let deployer: SignerWithAddress;
 let signers: SignerWithAddress[];
@@ -15,29 +16,31 @@ let precompileValidating: MockPrecompile;
 let usageValidating: MockPCUValidateDoubleSign;
 
 describe('[Precompile] Validate double sign test', () => {
+  let slasheeAddr: Address;
+  let header1 = ethers.utils.toUtf8Bytes('sampleHeader1');
+  let header2 = ethers.utils.toUtf8Bytes('sampleHeader2');
+
   before(async () => {
     [deployer, ...signers] = await ethers.getSigners();
 
     precompileValidating = await new MockPrecompile__factory(deployer).deploy();
     usageValidating = await new MockPCUValidateDoubleSign__factory(deployer).deploy(precompileValidating.address);
-  });
 
-  let consensusAddr = signers[0].address;
-  let header1 = ethers.utils.toUtf8Bytes('sampleHeader1');
-  let header2 = ethers.utils.toUtf8Bytes('sampleHeader2');
+    slasheeAddr = signers[0].address;
+  });
 
   it('Should the usage contract correctly configs the precompile address', async () => {
     expect(await usageValidating.precompileValidateDoubleSignAddress()).eq(precompileValidating.address);
   });
 
   it('Should the usage contract can call the precompile address', async () => {
-    let sortedValidators = await usageValidating.callPrecompile(consensusAddr, header1, header2);
+    let sortedValidators = await usageValidating.callPrecompile(slasheeAddr, header1, header2);
     expect(sortedValidators).eql(true);
   });
 
   it('Should the usage contract revert with proper message on calling the precompile contract fails', async () => {
     await usageValidating.setPrecompileValidateDoubleSignAddress(ethers.constants.AddressZero);
-    await expect(usageValidating.callPrecompile(consensusAddr, header1, header2)).revertedWithCustomError(
+    await expect(usageValidating.callPrecompile(slasheeAddr, header1, header2)).revertedWithCustomError(
       usageValidating,
       'ErrCallPrecompiled'
     );
