ab8b14dc74afd76bfafe9fd8565d80ad5abfbebc
nxqbao <quocbao300@gmail.com>
2023-03-09 15:13:24 +0700

    Fix gas addition gas to be 1200 (3500 in total)

--- contracts/extensions/consumers/GlobalConfigConsumer.sol
+++ contracts/extensions/consumers/GlobalConfigConsumer.sol
@@ -3,8 +3,8 @@
 pragma solidity ^0.8.9;
 
 abstract contract GlobalConfigConsumer {
-  /// The addition amount of gas stipend send along in external calls.
-  uint256 public constant DEFAULT_ADDITION_GAS = 3500;
+  /// @dev The addition amount of gas sending along in external calls. Total gas stipend is added with default 2300 gas.
+  uint256 public constant DEFAULT_ADDITION_GAS = 1200;
   /// @dev The length of a period in second.
   uint256 public constant PERIOD_DURATION = 1 days;
 }
