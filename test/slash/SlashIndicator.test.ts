14e9eda2352ba391a4a7007909740c5d1433a4fa
TuDo1403 <tudo.dev@gmail.com>
2023-05-16 18:08:56 +0700

    feat: change onlyAdmin require string to revert custom

--- test/slash/SlashIndicator.test.ts
+++ test/slash/SlashIndicator.test.ts
@@ -449,7 +449,7 @@ describe('Slash indicator test', () => {
           .connect(validatorCandidates[slasherIdx].consensusAddr)
           .slashDoubleSign(validatorCandidates[slasherIdx].consensusAddr.address, header1, header2);
 
-        await expect(tx).revertedWith('HasProxyAdmin: unauthorized sender');
+        await expect(tx).revertedWithCustomError(slashContract, 'Unauthorized');
       });
 
       it('Should non-admin not be able to slash validator with double signing', async () => {
@@ -465,7 +465,7 @@ describe('Slash indicator test', () => {
           .connect(validatorCandidates[slasherIdx].consensusAddr)
           .slashDoubleSign(validatorCandidates[slasheeIdx].consensusAddr.address, header1, header2);
 
-        await expect(tx).revertedWith('HasProxyAdmin: unauthorized sender');
+        await expect(tx).revertedWithCustomError(slashContract, 'Unauthorized');
       });
 
       it('Should be able to slash validator with double signing', async () => {
