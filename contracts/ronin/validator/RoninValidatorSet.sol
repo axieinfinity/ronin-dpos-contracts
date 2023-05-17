217818948e0ecaf8e4c21c3d8a468c39cc3025ff
TuDo1403 <tudo.dev@gmail.com>
2023-05-17 15:40:13 +0700

    fix: remove payable constructor

--- contracts/ronin/validator/RoninValidatorSet.sol
+++ contracts/ronin/validator/RoninValidatorSet.sol
@@ -8,7 +8,7 @@ import "./CoinbaseExecution.sol";
 import "./SlashingExecution.sol";
 
 contract RoninValidatorSet is Initializable, CoinbaseExecution, SlashingExecution {
-  constructor() payable {
+  constructor() {
     _disableInitializers();
   }
 
