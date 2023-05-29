b857c57f0ae96b12fd2161647da3d556dd8693e4
nxqbao <quocbao300@gmail.com>
2023-05-29 13:24:37 +0700

    fix deploy script

---
+++ src/deploy/proxy/profile-proxy.ts
@@ -0,0 +1,33 @@
+import { network } from 'hardhat';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+import { generalRoninConf, roninchainNetworks } from '../../configs/config';
+import { verifyAddress } from '../../script/verify-address';
+import { Profile__factory } from '../../types';
+
+const deploy = async ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) => {
+  if (!roninchainNetworks.includes(network.name!)) {
+    return;
+  }
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const logicContract = await deployments.get('ProfileLogic');
+
+  const data = new Profile__factory().interface.encodeFunctionData('initialize', [
+    generalRoninConf[network.name]!.stakingContract?.address,
+  ]);
+
+  const deployment = await deploy('ProfileProxy', {
+    contract: 'TransparentUpgradeableProxyV2',
+    from: deployer,
+    log: true,
+    args: [logicContract.address, generalRoninConf[network.name]!.governanceAdmin?.address, data],
+  });
+};
+
+deploy.tags = ['ProfileProxy'];
+deploy.dependencies = ['ProfileLogic'];
+
+export default deploy;
