47aa169a922950045d9d113ad4343876b772f9c0
TuDo1403 <tudo.dev@gmail.com>
2023-06-18 01:21:39 +0700

    format: update natspec documentation

--- contracts/utils/version-control/RoninValidatorSetTImedMigrator.sol
+++ contracts/utils/version-control/RoninValidatorSetTImedMigrator.sol
@@ -5,7 +5,16 @@ import { ConditionalVersionControl } from "./ConditionalVersionControl.sol";
 import { ITimingInfo } from "../../interfaces/validator/info-fragments/ITimingInfo.sol";
 import { ICoinbaseExecution } from "../../interfaces/validator/ICoinbaseExecution.sol";
 
+/**
+ * @title RoninValidatorSetTimedMigrator
+ * @dev A contract that facilitates timed migration of the Ronin validator set using conditional version control.
+ */
 contract RoninValidatorSetTimedMigrator is ConditionalVersionControl {
+  /**
+   * @dev Modifier that executes the function when conditions are met.
+   * @dev If the function is wrapUpEpoch from ICoinbaseExecution, it checks the current period before and after execution.
+   * If they differ, it triggers the selfMigrate function.
+   */
   modifier whenConditionsAreMet() override {
     if (msg.sig == ICoinbaseExecution.wrapUpEpoch.selector) {
       uint256 currentPeriod = _getCurrentPeriod();
@@ -18,16 +27,30 @@ contract RoninValidatorSetTimedMigrator is ConditionalVersionControl {
     }
   }
 
+  /**
+   * @dev Constructs the RoninValidatorSetTimedMigrator contract.
+   * @param proxyStorage The address of the proxy storage contract.
+   * @param currentVersion The address of the current contract implementation.
+   * @param newVersion The address of the new contract implementation.
+   */
   constructor(
     address proxyStorage,
     address currentVersion,
     address newVersion
   ) ConditionalVersionControl(proxyStorage, currentVersion, newVersion) {}
 
+  /**
+   * @dev Internal function to choose the current version of the contract implementation.
+   * @return The address of the current version implementation.
+   */
   function _chooseVersion() internal view override returns (address) {
     return _currentVersion;
   }
 
+  /**
+   * @dev Internal function to get the current period from ITimingInfo.
+   * @return The current period.
+   */
   function _getCurrentPeriod() private view returns (uint256) {
     return ITimingInfo(address(this)).currentPeriod();
   }
