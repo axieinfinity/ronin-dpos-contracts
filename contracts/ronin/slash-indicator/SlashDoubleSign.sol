23eb77cef9662501b9152361965eafa38dcd7146
nxqbao <quocbao300@gmail.com>
2023-06-21 16:31:31 +0700

    fix edge case

--- contracts/ronin/slash-indicator/SlashDoubleSign.sol
+++ contracts/ronin/slash-indicator/SlashDoubleSign.sol
@@ -42,6 +42,13 @@ abstract contract SlashDoubleSign is ISlashDoubleSign, HasContracts, HasValidato
     }
 
     address validatorId = _convertC2P(consensusAddr);
+
+    // Edge case: non-validator who never apply for the candidate role, nor have a profile.
+    // Must be slashed by the consensus address, since the validatorId will be address(0).
+    if (validatorId == address(0)) {
+      validatorId = TConsensus.unwrap(consensusAddr);
+    }
+
     if (_pcValidateEvidence(validatorId, header1, header2)) {
       IRoninValidatorSet validatorContract = IRoninValidatorSet(getContract(ContractType.VALIDATOR));
       uint256 period = validatorContract.currentPeriod();
