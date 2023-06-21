04b605c20e22d722b6b1e94c1a8f8b7b2b0b2c4f
Tu Do <tudo.dev@gmail.com>
2023-06-21 13:48:07 +0700

    Update contracts/extensions/version-control/ConditionalImplementControl.sol
    Co-authored-by: Bao <quocbao300@gmail.com>

--- contracts/extensions/version-control/ConditionalImplementControl.sol
+++ contracts/extensions/version-control/ConditionalImplementControl.sol
@@ -155,9 +155,8 @@ abstract contract ConditionalImplementControl is IConditionalImplementControl, E
    * @dev Suggested gas stipend for contract to call {selfMigrate} function.
    */
   function _gasStipenedNoGrief() internal pure virtual returns (uint256) {
-    // Gas stipend for contract to perform a few
-    // storage reads and writes, but low enough to prevent griefing.
-    // Griefing means comsuming gas exhaustively when function call are reverted.
+    // Gas stipend for contract to perform a few read and write operations on storage, but
+    // low enough to prevent comsuming gas exhaustively when function call are reverted.
     // Multiply by a small constant (e.g. 2), if needed.
     return 50_000;
   }
