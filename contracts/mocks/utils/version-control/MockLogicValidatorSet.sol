3c3ea945ef695d50eb4aceb584cb77765273d6fe
Tu Do <tudo.dev@gmail.com>
2023-06-21 10:34:38 +0700

    Update contracts/mocks/utils/version-control/MockLogicValidatorSet.sol
    Co-authored-by: Bao <quocbao300@gmail.com>

--- contracts/mocks/utils/version-control/MockLogicValidatorSet.sol
+++ contracts/mocks/utils/version-control/MockLogicValidatorSet.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { GlobalConfigConsumer } from "../../../extensions/consumers/GlobalConfigConsumer.sol";
 
 interface ILogicValidatorSet {
   function wrapUpEpoch() external payable;
