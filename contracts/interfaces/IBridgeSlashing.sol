5d4c14e710a643438f340d6a784b46a5a1b5a21b
TuDo1403 <tudo.dev@gmail.com>
2023-07-12 11:07:45 +0700

    format: add comments, minor refactor

--- contracts/interfaces/IBridgeSlashing.sol
+++ contracts/interfaces/IBridgeSlashing.sol
@@ -1,16 +1,53 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+/**
+ * @title IBridgeSlashing
+ * @dev Interface for the SlashBridgeIndicator contract to manage slashing functionality for bridge operators.
+ */
 interface IBridgeSlashing {
+  /**
+   * @dev Enumeration representing the slashing tiers for bridge operators.
+   */
   enum Tier {
     Tier0,
     Tier1,
     Tier2
   }
 
+  /**
+   * @dev Struct representing the status of a bridge operator.
+   */
+  struct BridgeOperatorStatus {
+    uint64 penalizedDuration;
+    uint192 newlyAddedAtPeriod;
+  }
+
+  /**
+   * @dev Event emitted when a bridge operator is slashed.
+   * @param tier The slash tier of the operator.
+   * @param bridgeOperator The address of the slashed bridge operator.
+   * @param period The period in which the operator is slashed.
+   * @param until The timestamp until which the operator is penalized.
+   */
   event Slashed(Tier indexed tier, address indexed bridgeOperator, uint256 indexed period, uint256 until);
 
+  /**
+   * @dev Slashes the unavailability of bridge operators during a specific period.
+   * @param period The period to slash the bridge operators for.
+   */
   function slashUnavailability(uint256 period) external;
 
+  function TIER_1_PENALIZE_DURATION() external view returns (uint256);
+
+  function TIER_2_PENALIZE_DURATION() external view returns (uint256);
+
+  function REMOVE_DURATION_THRESHOLD() external view returns (uint256);
+
+  /**
+   * @dev Returns the penalize durations for the specified bridge operators.
+   * @param bridgeOperators The addresses of the bridge operators.
+   * @return durations The penalized durations for the bridge operators.
+   */
   function penalizeDurationOf(address[] calldata bridgeOperators) external returns (uint256[] memory durations);
 }
--- contracts/ronin/gateway/SlashBridgeIndicator.sol
+++ contracts/ronin/gateway/SlashBridgeIndicator.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.17;
 
 import { Initializable } from "@openzeppelin/contracts/proxy/utils/Initializable.sol";
-import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { HasContracts } from "../../extensions/collections/HasContracts.sol";
 import { IBridgeSlashing } from "../../interfaces/IBridgeSlashing.sol";
 import { IBridgeManager } from "../../interfaces/IBridgeManager.sol";
@@ -11,8 +10,17 @@ import { IBridgeTracking } from "../../interfaces/IBridgeTracking.sol";
 import { IRoninValidatorSet } from "../../interfaces/validator/IRoninValidatorSet.sol";
 import { ContractType } from "../../utils/ContractType.sol";
 
+/**
+ * @title SlashBridgeIndicator
+ * @dev A contract that implements slashing functionality for bridge operators based on their availability.
+ */
 contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initializable, HasContracts {
-  using EnumerableSet for EnumerableSet.AddressSet;
+  /// @inheritdoc IBridgeSlashing
+  uint256 public constant TIER_1_PENALIZE_DURATION = 1 days;
+  /// @inheritdoc IBridgeSlashing
+  uint256 public constant TIER_2_PENALIZE_DURATION = 5 days;
+  /// @inheritdoc IBridgeSlashing
+  uint256 public constant REMOVE_DURATION_THRESHOLD = 30 days;
 
   /// @dev Tier 1 slashing threshold ratio is 10%
   uint256 private constant TIER_1_THRESHOLD = 10_00;
@@ -20,12 +28,9 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
   uint256 private constant TIER_2_THRESHOLD = 30_00;
   /// @dev Max percentage 100%. Values [0; 100_00] reflexes [0; 100%]
   uint256 private constant PERCENTAGE_FRACTION = 100_00;
-  uint256 private constant TIER_1_PENALIZE_DURATION = 1 days;
-  uint256 private constant TIER_2_PENALIZE_DURATION = 5 days;
-  uint256 private constant REMOVE_DURATION_THRESHOLD = 30 days;
-
-  mapping(address => uint256) private _addedPeriod;
-  mapping(address => uint256) private _penalizedDurationsOf;
+  /// @dev value is equal to keccak256("@ronin.dpos.gateway.SlashBridgeIndicator.bridgeOperatorStatuses.slot") - 1
+  bytes32 private constant BRIDGE_OPERATOR_STATUSES_SLOT =
+    0x315ed8a0abb9fd55e40c49aa52c641ee78f97b4f2e33534e1bafd5daaa763881;
 
   function initialize(
     address validatorContract,
@@ -37,15 +42,19 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
     _setContract(ContractType.BRIDGE_TRACKING, bridgeTrackingContract);
   }
 
+  /**
+   * @inheritdoc IBridgeManagerCallback
+   */
   function onBridgeOperatorsAdded(
     address[] calldata bridgeOperators,
     bool[] memory addeds
   ) external onlyContract(ContractType.BRIDGE_MANAGER) returns (bytes4) {
     uint256 length = bridgeOperators.length;
     uint256 currentPeriod = IRoninValidatorSet(getContract(ContractType.VALIDATOR)).currentPeriod();
+    mapping(address => BridgeOperatorStatus) storage _bridgeOperatorStatuses = _getBridgeOperatorStatuses();
     for (uint256 i; i < length; ) {
       if (addeds[i]) {
-        _addedPeriod[bridgeOperators[i]] = currentPeriod;
+        _bridgeOperatorStatuses[bridgeOperators[i]].newlyAddedAtPeriod = uint192(currentPeriod);
       }
       unchecked {
         ++i;
@@ -55,6 +64,9 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
     return IBridgeManagerCallback.onBridgeOperatorsAdded.selector;
   }
 
+  /**
+   * @inheritdoc IBridgeManagerCallback
+   */
   function onBridgeOperatorsRemoved(
     address[] calldata,
     bool[] calldata
@@ -62,6 +74,9 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
     return IBridgeManagerCallback.onBridgeOperatorsAdded.selector;
   }
 
+  /**
+   * @inheritdoc IBridgeManagerCallback
+   */
   function onBridgeOperatorUpdated(
     address,
     address,
@@ -70,40 +85,57 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
     return IBridgeManagerCallback.onBridgeOperatorUpdated.selector;
   }
 
+  /**
+   * @inheritdoc IBridgeSlashing
+   */
   function slashUnavailability(uint256 period) external onlyContract(ContractType.BRIDGE_TRACKING) {
-    IBridgeTracking bridgeTracker = IBridgeTracking(msg.sender);
     IBridgeManager bridgeManager = IBridgeManager(getContract(ContractType.BRIDGE_MANAGER));
 
-    uint256[] memory penalizedDurations = _getPenalizedDurations();
-    address[] memory allBridgeOperators = bridgeManager.getBridgeOperators();
-    uint256[] memory ballots = bridgeTracker.getManyTotalBallots(period, allBridgeOperators);
+    uint256 totalBallotsForPeriod;
+    uint256[] memory ballots;
+    address[] memory allBridgeOperators;
+    // get rid of stack too deep
+    {
+      allBridgeOperators = bridgeManager.getBridgeOperators();
+      IBridgeTracking bridgeTracker = IBridgeTracking(msg.sender);
+      totalBallotsForPeriod = bridgeTracker.totalBallots(period);
+      ballots = bridgeTracker.getManyTotalBallots(period, allBridgeOperators);
+    }
+
     uint256 length = allBridgeOperators.length;
     address[] memory bridgeOperatorsToRemoved = new address[](length);
-    uint256 totalBallotsForPeriod = bridgeTracker.totalBallots(period);
-
-    uint256 penalizedDuration;
-    address bridgeOperator;
     uint256 removeLength;
-    Tier tier;
+    {
+      uint256[] memory penalizedDurations = _getPenalizedDurations();
+      mapping(address => BridgeOperatorStatus) storage _bridgeOperatorStatuses = _getBridgeOperatorStatuses();
 
-    for (uint256 i; i < length; ) {
-      bridgeOperator = allBridgeOperators[i];
-      if (_addedPeriod[bridgeOperator] < period) {
-        tier = _getSlashTier(ballots[i], totalBallotsForPeriod);
-        penalizedDuration = _penalizedDurationsOf[bridgeOperator] + penalizedDurations[uint8(tier)];
+      BridgeOperatorStatus memory status;
+      uint256 penalizedDuration;
+      address bridgeOperator;
+      Tier tier;
 
-        if (penalizedDuration >= REMOVE_DURATION_THRESHOLD) {
-          bridgeOperatorsToRemoved[removeLength] = bridgeOperator;
-          ++removeLength;
+      for (uint256 i; i < length; ) {
+        bridgeOperator = allBridgeOperators[i];
+        status = _bridgeOperatorStatuses[bridgeOperator];
+
+        if (status.newlyAddedAtPeriod < period) {
+          tier = _getSlashTier(ballots[i], totalBallotsForPeriod);
+          penalizedDuration = status.penalizedDuration + penalizedDurations[uint8(tier)];
+
+          if (penalizedDuration >= REMOVE_DURATION_THRESHOLD) {
+            bridgeOperatorsToRemoved[removeLength] = bridgeOperator;
+            ++removeLength;
+          }
+
+          status.penalizedDuration = uint64(penalizedDuration);
+          _bridgeOperatorStatuses[bridgeOperator] = status;
+
+          emit Slashed(tier, bridgeOperator, period, block.timestamp + penalizedDuration);
         }
 
-        _penalizedDurationsOf[bridgeOperator] = penalizedDuration;
-
-        emit Slashed(tier, bridgeOperator, period, block.timestamp + penalizedDuration);
-      }
-
-      unchecked {
-        ++i;
+        unchecked {
+          ++i;
+        }
       }
     }
 
@@ -115,20 +147,44 @@ contract SlashBridgeIndicator is IBridgeSlashing, IBridgeManagerCallback, Initia
     bridgeManager.removeBridgeOperators(bridgeOperatorsToRemoved);
   }
 
+  /**
+   * @inheritdoc IBridgeSlashing
+   */
   function penalizeDurationOf(address[] calldata bridgeOperators) external view returns (uint256[] memory durations) {
     uint256 length = bridgeOperators.length;
     durations = new uint256[](length);
+    mapping(address => BridgeOperatorStatus) storage _bridgeOperatorStatuses = _getBridgeOperatorStatuses();
     for (uint256 i; i < length; ) {
-      durations[i] = _penalizedDurationsOf[bridgeOperators[i]];
+      durations[i] = _bridgeOperatorStatuses[bridgeOperators[i]].penalizedDuration;
       unchecked {
         ++i;
       }
     }
   }
 
+  /**
+   * @dev Gets the slash tier based on the given ballot and total ballots.
+   * @param ballot The ballot count for a bridge operator.
+   * @param totalBallots The total ballot count for the period.
+   * @return tier The slash tier.
+   */
   function _getSlashTier(uint256 ballot, uint256 totalBallots) internal pure virtual returns (Tier tier) {
     uint256 ratio = (ballot * PERCENTAGE_FRACTION) / totalBallots;
     tier = ratio > TIER_2_PENALIZE_DURATION ? Tier.Tier2 : ratio > TIER_1_PENALIZE_DURATION ? Tier.Tier1 : Tier.Tier0;
+  }
+
+  /**
+   * @dev Internal function to access the mapping from bridge operator => BridgeOperatorStatus.
+   * @return bridgeOperatorStatuses_ the mapping from governor => BridgeOperatorInfo.
+   */
+  function _getBridgeOperatorStatuses()
+    internal
+    pure
+    returns (mapping(address => BridgeOperatorStatus) storage bridgeOperatorStatuses_)
+  {
+    assembly {
+      bridgeOperatorStatuses_.slot := BRIDGE_OPERATOR_STATUSES_SLOT
+    }
   }
 
   function _getPenalizedDurations() internal pure virtual returns (uint256[] memory penalizedDurations) {
