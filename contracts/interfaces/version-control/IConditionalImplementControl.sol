a0925fd4b51f2a90de5e2f8c09bd8e2e95ebf208
TuDo1403 <tudo.dev@gmail.com>
2023-07-13 16:15:25 +0700

    format: remove duplicate `ErrZeroCodeContract` definition

--- contracts/interfaces/version-control/IConditionalImplementControl.sol
+++ contracts/interfaces/version-control/IConditionalImplementControl.sol
@@ -2,8 +2,6 @@
 pragma solidity ^0.8.0;
 
 interface IConditionalImplementControl {
-  /// @dev Error of set to non-contract.
-  error ErrZeroCodeContract(address addr);
   /// @dev Error when contract which delegate to this contract is not compatible with ERC1967
   error ErrDelegateFromUnknownOrigin(address addr);
 
