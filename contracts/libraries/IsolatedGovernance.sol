966d1b8283ea1bd4584ab4feb4c75ccb1cdc85df
TuDo1403 <tudo.dev@gmail.com>
2023-07-19 15:18:26 +0700

    format: remove `Strings` lib

--- contracts/libraries/IsolatedGovernance.sol
+++ contracts/libraries/IsolatedGovernance.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Strings.sol";
 import "../interfaces/consumers/VoteStatusConsumer.sol";
 import "../utils/CommonErrors.sol";
 
