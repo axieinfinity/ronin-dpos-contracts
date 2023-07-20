ed49212c3924833d705de522243838300a6adc16
nxqbao <quocbao300@gmail.com>
2023-07-20 16:37:29 +0700

    init commit

---
+++ contracts/ronin/profile/ProfileStorage.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
+
+import "../../extensions/collections/HasContracts.sol";
+import { IProfile } from "../../interfaces/IProfile.sol";
+
+abstract contract ProfileStorage is IProfile, HasContracts {
+  /// @dev Mapping from id address => candidate profile.
+  mapping(address => CandidateProfile) internal _id2Profile;
+  /// @dev Upgradeable gap.
+  bytes32[50] __gap;
+
+  /**
+   * @dev Add a profile from memory to storage.
+   */
+  function _addNewProfile(CandidateProfile storage _profile, CandidateProfile memory newProfile) internal {
+    _profile.id = newProfile.id;
+    _profile.consensus = newProfile.consensus;
+    _profile.admin = newProfile.admin;
+    _profile.treasury = newProfile.treasury;
+    _profile.governor = newProfile.governor;
+    _profile.pubkey = newProfile.pubkey;
+
+    emit ProfileAdded(newProfile.id);
+  }
+
+  /**
+   * @dev Get an existed profile struct from id. Revert if the profile does not exists.
+   */
+  function _getId2ProfileHelper(address id) internal view returns (CandidateProfile storage _profile) {
+    _profile = _id2Profile[id];
+    if (_profile.id == address(0)) revert ErrNonExistentProfile();
+  }
+}
