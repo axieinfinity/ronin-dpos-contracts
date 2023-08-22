2400a84f18018ea10c694a75c534162543219a85
nxqbao <quocbao300@gmail.com>
2023-08-22 22:47:13 +0700

    add comment

--- contracts/interfaces/IFastFinalityTracking.sol
+++ contracts/interfaces/IFastFinalityTracking.sol
@@ -3,8 +3,17 @@
 pragma solidity ^0.8.9;
 
 interface IFastFinalityTracking {
+  /**
+   * @dev Submit list of `voters` who vote for fast finality in the current block.
+   *
+   * Requirements:
+   * - Only called once per block
+   */
   function recordFinality(address[] calldata voters) external;
 
+  /**
+   * @dev Returns vote count of `addrs` in the `period`.
+   */
   function getManyFinalityVoteCounts(
     uint256 period,
     address[] calldata addrs
