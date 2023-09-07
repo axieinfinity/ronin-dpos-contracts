9d4c91a74a0179b9ea9f4c1cd412e0871b3b991b
nxqbao <quocbao300@gmail.com>
2023-09-07 17:24:48 +0700

    fix fast finality tracking deploy script, add depl

--- src/deploy/proxy/fast-finality-tracking-proxy.ts
+++ src/deploy/proxy/fast-finality-tracking-proxy.ts
@@ -4,6 +4,8 @@ import { HardhatRuntimeEnvironment } from 'hardhat/types';
 import { generalRoninConf, roninchainNetworks } from '../../configs/config';
 import { verifyAddress } from '../../script/verify-address';
 import { FastFinalityTracking__factory } from '../../types';
+import { Address } from 'hardhat-deploy/dist/types';
+import { Network } from '../../utils';
 
 const deploy = async ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironment) => {
   if (!roninchainNetworks.includes(network.name!)) {
@@ -15,21 +17,39 @@ const deploy = async ({ getNamedAccounts, deployments }: HardhatRuntimeEnvironme
 
   const logicContract = await deployments.get('FastFinalityTrackingLogic');
 
+  let validatorContractAddress: Address;
+  let governanceAdmin: Address;
+  let nonce: number | undefined;
+  if (network.name == Network.Hardhat) {
+    nonce = generalRoninConf[network.name].fastFinalityTrackingContract?.nonce!;
+
+    validatorContractAddress = generalRoninConf[network.name]!.validatorContract?.address!;
+    governanceAdmin = generalRoninConf[network.name]!.governanceAdmin?.address!;
+  } else {
+    const validatorContractDeployment = await deployments.get('RoninValidatorSetProxy');
+    validatorContractAddress = validatorContractDeployment.address;
+
+    const GADepl = await deployments.get('RoninGovernanceAdmin');
+    governanceAdmin = GADepl.address;
+  }
+
   const data = new FastFinalityTracking__factory().interface.encodeFunctionData('initialize', [
-    generalRoninConf[network.name]!.validatorContract?.address,
+    validatorContractAddress,
   ]);
 
-  const nonce = generalRoninConf[network.name].fastFinalityTrackingContract?.nonce;
   // console.log(`Deploying FastFinalityTrackingProxy (nonce: ${nonce})...`);
 
   const deployment = await deploy('FastFinalityTrackingProxy', {
     contract: 'TransparentUpgradeableProxyV2',
     from: deployer,
     log: true,
-    args: [logicContract.address, generalRoninConf[network.name]!.governanceAdmin?.address, data],
+    args: [logicContract.address, governanceAdmin, data],
     nonce,
   });
-  verifyAddress(deployment.address, generalRoninConf[network.name].fastFinalityTrackingContract?.address);
+
+  if (network.name == Network.Hardhat) {
+    verifyAddress(deployment.address, generalRoninConf[network.name].fastFinalityTrackingContract?.address);
+  }
 };
 
 deploy.tags = ['FastFinalityTrackingProxy'];
